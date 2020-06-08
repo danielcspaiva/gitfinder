70283f83979573166950ffc8ee45f2e0e1521abe
Daniel Paiva <danielcspaiva@gmail.com>
2020-06-08 01:00:12 -0300

    added card animations

--- src/components/views/Home.js
+++ src/components/views/Home.js
@@ -3,7 +3,8 @@ import axios from "axios";
 import Navbar from "../Navbar";
 import Footer from "../Footer";
 import UserCard from "../UserCard";
-import RepoCard from '../RepoCard';
+import RepoCard from "../RepoCard";
+import SyncLoader from "react-spinners/SyncLoader";
 
 export default function Home() {
   const [search, setSearch] = useState("");
@@ -12,20 +13,34 @@ export default function Home() {
   const repoBaseUrl = "https://api.github.com/search/repositories?q=";
   const userBaseUrl = "https://api.github.com/search/users?q=";
   const [searchUrl, setSearchUrl] = useState(userBaseUrl);
+  const [loading, setLoading] = useState(false);
+  console.log(data);
 
   useEffect(() => {
     const { cancel } = axios.CancelToken.source();
-    searchKind === "user"
-      ? setSearchUrl(userBaseUrl)
-      : setSearchUrl(repoBaseUrl);
+    if (search === "") {
+      setLoading(false);
+      setData([]);
+      return;
+    }
+    setLoading(true);
     const timeOutId = setTimeout(() => {
       axios
         .get(searchUrl + search)
-        .then((data) => setData(data.data.items))
+        .then((data) => {
+          setData(data.data.items);
+          setLoading(false);
+        })
         .catch((err) => console.log(err));
     }, 500);
     return () => cancel("no longer latest query") || clearTimeout(timeOutId);
-  }, [search, searchKind, searchUrl]);
+  }, [search, searchKind]);
+
+  const handleSearchKind = (kind) => {
+    setData([]);
+    setSearchKind(kind);
+    kind === "user" ? setSearchUrl(userBaseUrl) : setSearchUrl(repoBaseUrl);
+  };
 
   const updateSearch = (e) => {
     setSearch(e.target.value);
@@ -37,13 +52,13 @@ export default function Home() {
         <Navbar />
         <div className="btn-container">
           <button
-            onClick={() => setSearchKind("user")}
+            onClick={() => handleSearchKind("user")}
             className={searchKind === "user" ? "active" : "inactive"}
           >
             Users
           </button>
           <button
-            onClick={() => setSearchKind("repo")}
+            onClick={() => handleSearchKind("repo")}
             className={searchKind === "repo" ? "active" : "inactive"}
           >
             Repositories
@@ -58,11 +73,19 @@ export default function Home() {
         />
       </div>
       <div className="grid">
-        {searchKind === "user"
-          ? data.map((user) => <UserCard key={user.id} user={user} />)
-          : data.map((repo) => <RepoCard key={repo.id} repo={repo} />)}
+        {data.length ? (
+          searchKind === "user" ? (
+            data.map((user) => <UserCard key={user.id} user={user} />)
+          ) : (
+            data.map((repo) => <RepoCard key={repo.id} repo={repo} />)
+          )
+        ) : (
+          <div className="loading-container">
+            <SyncLoader size={30} color={"#3D2992"} loading={loading} />
+          </div>
+        )}
+        <Footer />
       </div>
-      <Footer />
     </div>
   );
 }
